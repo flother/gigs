80b071bcfdbdfb285c3194e3cae9320097d5452a
Matt Riggott <matt.riggott@gmail.com>
2010-02-16 18:14:28 +0000

    Add lots more loveliness to the gigs home page.
    The loveliness includes lists of artists, venues, and towns (the first two ordered by popularity), and thumbnail images for gigs in the next two weeks.

--- views.py
+++ views.py
@@ -3,21 +3,28 @@ import datetime
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from gigs.models import Gig
+from gigs.models import Gig, Artist, Venue, Town
 
 
 def home_page(request):
     """
-    Show lists of gigs happening this week and the week after.  It also
-    includes the eight soonest occurring gigs in a separate list, along
-    with the fifteen most recently added gigs.
+    Lots of lovely lists to give the visitor an idea of what's happening
+    soon and how they can browse the site.
+
+    The view provides the template with the eight soonest occurring gigs,
+    gigs happening this week and next, the fifteen most recently added
+    gigs, and artists, venues, and towns.
     """
     today = datetime.date.today()
     one_week = datetime.timedelta(days=7)
     start_of_this_week = today - datetime.timedelta(days=today.weekday())
-    start_of_next_week = today + one_week
+    start_of_next_week = start_of_this_week + one_week
     start_of_week_after_next = start_of_next_week + one_week
 
+    # Create four lists: the eight gigs happening soonest, gigs happening this
+    # week (Monday to Sunday; ignoring days already passed), gigs happening next
+    # week (Monday to Sunday), and the 15 gigs most recently added to the
+    # database.
     closest_gigs = Gig.objects.published(date__gte=today).select_related()[:8]
     gigs_this_week = Gig.objects.published(date__gte=today,
         date__lt=start_of_next_week).select_related()
@@ -26,11 +33,27 @@ def home_page(request):
     new_gigs = Gig.objects.published(date__gte=today).order_by(
         '-created').select_related()[:15]
 
+    # Create lists of artists and venues, those with the largest number of
+    # upcoming gigs first, all towns, and the number of gigs at each venue and
+    # for each artist.
+    artists = Artist.objects.all().order_by('-number_of_upcoming_gigs', '?')[:11]
+    number_of_artists = Artist.objects.count()
+    venues = Venue.objects.all().order_by('-number_of_upcoming_gigs')[:11]
+    number_of_venues = Venue.objects.count()
+    towns = Town.objects.all()
+    number_of_towns = Town.objects.count()
+
     context = {
         'closest_gigs': closest_gigs,
         'gigs_this_week': gigs_this_week,
         'gigs_next_week': gigs_next_week,
         'new_gigs': new_gigs,
+        'artists': artists,
+        'venues': venues,
+        'towns': towns,
+        'number_of_artists': number_of_artists,
+        'number_of_venues': number_of_venues,
+        'number_of_towns': number_of_towns,
     }
     return render_to_response('gigs/home_page.html', context,
         RequestContext(request))
