9a69603204a9341817b0c6449f2d55dfff8a5888
Matt Riggott <matt.riggott@gmail.com>
2010-02-21 16:36:48 +0000

    Added a view and template to list all towns.

--- urls.py
+++ urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.list_detail import object_list
 
+from gigs.models import Town
 from gigs import views
+
+
+town_list_dict = {
+    'queryset': Town.objects.all(),
+    'allow_empty': True,
+    'template_object_name': 'town',
+}
 
 
 urlpatterns = patterns('',
     url(r'^$', views.home_page, name='gigs_home_page'),
     url(r'^artists/$', views.artist_list, name='gigs_artist_list'),
     url(r'^venues/$', views.venue_list, name='gigs_venue_list'),
+    url(r'^towns/$', object_list, town_list_dict, name='gigs_town_list'),
 )
