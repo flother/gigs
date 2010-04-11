9e5167144af8e7d1ecf590b72d2a0e65cfdba855
Matt Riggott <matt.riggott@gmail.com>
2010-04-11 21:23:48 +0100

    Reduced the default number of characters kept in the truncate filter from 17 to 15.

--- templatetags/typography.py
+++ templatetags/typography.py
@@ -7,19 +7,19 @@ register = template.Library()
 
 @register.filter
 @stringfilter
-def truncate(value, max_length=17):
+def truncate(value, max_length=15):
     """
     Return a string, truncated if it exceeds the ``max_length`` argument
     in length.  If truncated, the string will be ``max_length`` characters
     long, the last being a horizontal ellipsis.  If no truncation was
     necessary the original string is returned.
 
-    >>> truncate('12345678901234567')
-    '12345678901234567'
-    >>> truncate('123456789012345678')
-    '1234567890123456\u2026'
+    >>> truncate('123456789012345')
+    '123456789012345'
+    >>> truncate('1234567890123456')
+    '12345678901234\u2026'
     >>> truncate('This is a long sentence, with nothing to say')
-    u'This is a long s\u2026'
+    u'This is a long\u2026'
     >>> truncate('This is a long sentence, with nothing to say', 26)
     u'This is a long sentence,\u2026'
     """
